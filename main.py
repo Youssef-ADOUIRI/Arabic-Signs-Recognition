7d1993e3d4e413bd27d35db8d3593b1df18fec5d
Youssef-ADOUIRI <youssef.galaoui@gmail.com>
2022-02-17 16:58:01 +0100

    Test conda env and opencv packages

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import numpy as np
+import cv2
 
-M = np.zeros((2,3))
+M = np.ones((2,3), dtype=int) * 2
+print(M)
+
+cam = cv2.VideoCapture(0 , cv2.CAP_DSHOW)
+
+while True:
+    check, frame = cam.read()
+
+    cv2.imshow('video', frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
