cc884daf3644bfc91745b192caa2a22f4c92f324
Youssef-ADOUIRI <youssef.galaoui@gmail.com>
2022-04-08 05:03:39 +0000

    adding cv2 camera to flask api

--- web-app/Back-end/camera.py
+++ web-app/Back-end/camera.py
@@ -1,3 +1,4 @@
+import cv2
 class VideoCamera(object):
     def __init__(self):
       self.video = cv2.VideoCapture(0)
---
+++ web-app/Back-end/server.py
@@ -0,0 +1,38 @@
+from flask import Flask,Response,render_template
+import datetime
+from camera import VideoCamera as cam
+
+  
+x = datetime.datetime.now()
+  
+# Initializing flask app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.js')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(cam()),mimetype='multipart/x-mixed-replace; boundary=frame')  
+
+# Route for seeing a data
+@app.route('/data')
+def get_time():
+    # Returning an api for showing in  reactjs
+    return {
+        'Name':"Arabic signs", 
+        "Age":"22",
+        "Date":x, 
+        "programming":"python"
+        }
+  
+      
+# Running app
+if __name__ == '__main__':
+    app.run(debug=True , host='0.0.0.0', port=5000, threaded=True, use_reloader=False)
