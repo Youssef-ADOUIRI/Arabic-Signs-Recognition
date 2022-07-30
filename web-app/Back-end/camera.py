a17ff9ab0e71a53bad2d9b4267e9ef9b0e9d7ddb
Youssef-ADOUIRI <youssef.galaoui@gmail.com>
2022-07-30 21:17:21 +0100

    Deleting web-app from the project

--- web-app/Back-end/camera.py
+++
@@ -1,9 +0,0 @@
-import cv2
-class VideoCamera(object):
-    def __init__(self):
-      self.video = cv2.VideoCapture(0)
-    def __del__(self):
-      self.video.release()
-    def get_frame(self):
-      ret, frame = self.video.read()
-      return frame
